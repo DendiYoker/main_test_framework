c0a63a2a3386fc1479e0f3533786034bb0c7ea0a
Den21rus <denis.yuryevich.p@gmail.com>
2024-07-06 17:25:15 +0300

    added a class for working with web elements

---
+++ core/web_element.py
@@ -0,0 +1,31 @@
+from selenium.common import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+
+from core import environment_settings as ES
+from core.logging import log
+
+
+class WebElementHelper:
+    """
+     Класс для работы с web элементом
+    """
+
+    def __init__(self, locator):
+        self.locator = locator
+
+    def find_element(self):
+        try:
+            return ES.driver_wait_short.until(EC.presence_of_element_located(self.locator))
+        except TimeoutException:
+            #ToDo подумать про проброску ошибок run_test
+            raise log(f'Элемент "{self.locator[1]}" не найден')
+
+    def send_keys(self, text):
+        self.find_element().send_keys(text)
+
+    def clear(self):
+        self.find_element().clear()
+
+    def click_button(self):
+        # ToDo добавить проверку на кликабельность
+        self.find_element().click()
